c671df27b359b0594ec72092b5f4fa4687b073b1
league <league@imac-24.attlocal.net>
2022-10-26 16:56:09 -0700

    greeter

---
+++ src/_03_print_and_popups/_1_greeter/Greeter.java
@@ -0,0 +1,8 @@
+package _03_print_and_popups._1_greeter;
+
+public class Greeter {
+public static void main(String[] args) {
+System.out.println("Hello World!");
+}
+}
+String input = JOptionPane.showInputDialog("What is your name?");
